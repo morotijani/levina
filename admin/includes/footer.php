b1d5111b276ca534661bbf537d4ad147960ad2f3
Tijani Moro <tjhackx111@gmail.com>
2025-06-30 13:37:16 +0000

    initial commit

---
+++ admin/includes/footer.php
@@ -0,0 +1,10 @@
+            
+            </main>
+
+        </div> 
+    </div>   
+    <script src="<?= PROOT; ?>admin/dist/js/bootstrap.bundle.min.js" integrity="sha384-ndDqU0Gzau9qJ1lfW4pNLlhNTkCfHzAVBReH9diLvGRem5+R9g2FzA8ZGN954O5Q" class="astro-vvvwv3sm"></script> 
+    <script src="https://cdn.jsdelivr.net/npm/chart.js@4.3.2/dist/chart.umd.js" integrity="sha384-eI7PSr3L1XLISH8JdDII5YN/njoSsxfbrkCTnJrzXt+ENP5MOVBxD+l6sEG4zoLp" crossorigin="anonymous" class="astro-vvvwv3sm"></script>
+    <script src="<?= PROOT; ?>admin/dist/js/dashboard.js" class="astro-vvvwv3sm"></script> 
+</body> 
+</html>
