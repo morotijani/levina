6563ce7d22b8e96fa0a65df067b91a94188d92b6
Tijani Moro <tjhackx111@gmail.com>
2025-06-30 11:58:35 +0000

    initial commit

--- app/account-billing.php
+++ app/account-billing.php
@@ -396,16 +396,16 @@
                 </section>
 
                 <!-- Billing address -->
-                <section class="card border-0 py-1 p-md-2 p-xl-3 p-xxl-4">
+                <!-- <section class="card border-0 py-1 p-md-2 p-xl-3 p-xxl-4">
                     <div class="card-body">
                         <div class="d-flex align-items-center pb-4 mt-sm-n1 mb-0 mb-lg-1 mb-xl-3">
                             <i class="ai-map-pin text-primary lead pe-1 me-2"></i>
                             <h2 class="h4 mb-0">Billing address</h2>
                         </div>
-                        <div class="row row-cols-1 row-cols-md-2 g-4">
+                        <div class="row row-cols-1 row-cols-md-2 g-4"> -->
 
                             <!-- Address (primary) -->
-                            <div class="col">
+                            <!-- <div class="col">
                                 <div class="card h-100 rounded-3 p-3 p-sm-4">
                                     <div class="d-flex align-items-center pb-2 mb-1">
                                         <h3 class="h6 text-nowrap text-truncate mb-0">Billing address #1</h3>
@@ -421,10 +421,10 @@
                                     </div>
                                     <p class="mb-0">314 Robinson Lane,<br>Wilmington, DE 19805,<br>USA</p>
                                 </div>
-                            </div>
+                            </div> -->
 
                             <!-- Address -->
-                            <div class="col">
+                            <!-- <div class="col">
                                 <div class="card h-100 rounded-3 p-3 p-sm-4">
                                     <div class="d-flex align-items-center pb-2 mb-1">
                                         <h3 class="h6 text-nowrap text-truncate mb-0">Billing address #2</h3>
@@ -439,10 +439,10 @@
                                     </div>
                                     <p class="mb-0">401 Magnetic Drive Unit 2,<br>Toronto, Ontario, M3J 3H9<br>Canada</p>
                                 </div>
-                            </div>
+                            </div> -->
 
                             <!-- Add address -->
-                            <div class="col">
+                            <!-- <div class="col">
                                 <div class="card h-100 justify-content-center align-items-center border-dashed rounded-3 py-5 px-3 px-sm-4">
                                     <a class="stretched-link d-flex align-items-center fw-semibold text-decoration-none my-sm-3" href="#addAddress" data-bs-toggle="modal">
                                         <i class="ai-circle-plus fs-xl me-2"></i>
@@ -460,7 +460,7 @@
                             <p class="mb-0">Your text location determines the taxes that are applied to your bill. <a href="#" class="alert-link">Learn more</a></p>
                         </div>
                     </div>
-                </section>
+                </section> -->
             </div>
         </div>
     </div>
